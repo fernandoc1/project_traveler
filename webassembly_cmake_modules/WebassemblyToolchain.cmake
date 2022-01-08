e1276b49ac7d7a9cae4ddd5eddf630ec8c0780aa
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2022-01-08 18:19:42 -0300

    Added lua source code.

--- webassembly_cmake_modules/WebassemblyToolchain.cmake
+++ webassembly_cmake_modules/WebassemblyToolchain.cmake
@@ -1,6 +1,8 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Generic)
 
+message(">>> EMSDK=$ENV{EMSDK}")
+
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER "emcc")
 SET(CMAKE_CXX_COMPILER "emcc")
@@ -17,7 +19,6 @@ SET(USE_FLAGS "\
 -s USE_SDL_MIXER=2 \
 -s USE_SDL_TTF=2 \
 -s USE_VORBIS=1 \
--s WASM=1 \
 ")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${USE_FLAGS}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${USE_FLAGS}")
@@ -34,9 +35,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # search headers and libraries in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-#SET(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/webassembly_cmake_modules/" "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/")
-#message(">>> CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}")
 
 SET(SDL2_INCLUDE_DIR "${EMSDK}/upstream/emscripten/cache/sysroot/include/SDL2")
 SET(SDL2MAIN_LIBRARY "")
