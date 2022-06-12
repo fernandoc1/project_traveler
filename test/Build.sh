60fe8b80b4ab33757fb8ef587d00b3670fd0ce7a
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2022-06-12 12:01:42 -0300

    Fixed lua to allow compiling as shared library

--- test/Build.sh
+++ test/Build.sh
@@ -10,13 +10,10 @@ rm -rf ${OUTPUT_DIR}
 mkdir ${OUTPUT_DIR}
 
 LIBTRAVELER=${OUTPUT_DIR}/libtraveler.so
-#LIBTRAVELER=${OUTPUT_DIR}/libtraveler.a
+LIBTRAVELER_LUA=${OUTPUT_DIR}/libtraveler_lua.so
 
-#OBJECT_FILES=$(find ../build -iname "*.o" | grep -v "lua" | grep -v "main.cpp.o")
-OBJECT_FILES=$(find ../build -iname "*.o" | grep -v "main.cpp.o")
-
-#g++ ${OBJECT_FILES} -fPIC -shared -o ${LIBTRAVELER}
-#ar rcs ${LIBTRAVELER} $(find ../build -iname "*.o" | grep -v "lua" | grep -v "main.o")
+OBJECT_FILES=$(find ../build -iname "*.o" | grep -v "lua" | grep -v "main.cpp.o")
+LUA_OBJECT_FILES=$(find ../build -iname "*.o" | grep "lua")
 
 LIBRARIES="glew glx openal opengl glesv2 ogg vorbis vorbisfile libpng sdl2 SDL2_image SDL2_ttf"
 INCLUDE_DIRS="-I../src/ -I../src/utils/src/ -I../src/luabind/ -I../webassembly_deps/lua/"
@@ -25,8 +22,9 @@ LIB_LINK_FLAGS=$(pkg-config --cflags --libs ${LIBRARIES})
 
 BATTLE_TEST_O=${OUTPUT_DIR}/battle_test.o
 
+g++ -Wl,--start-group ${OBJECT_FILES} ${LIB_LINK_FLAGS} -Wl,--end-group -fPIC -shared -o ${LIBTRAVELER}
+g++ -Wl,--start-group ${LUA_OBJECT_FILES} ${LIB_LINK_FLAGS} -Wl,--end-group -fPIC -shared -o ${LIBTRAVELER_LUA}
 g++ ${INCLUDE_DIRS} ${LIB_INCLUDE_FLAGS} -c battle_test.cpp -o ${BATTLE_TEST_O}
-#g++ ${LIB_LINK_FLAGS} ${BATTLE_TEST_O} ${LIBTRAVERLER} 
-g++ -Wl,--start-group ${BATTLE_TEST_O} ${OBJECT_FILES} ${LIB_LINK_FLAGS} -Wl,--end-group
+g++ -Wl,--start-group ${BATTLE_TEST_O} ${LIBTRAVELER} ${LIBTRAVELER_LUA} ${LIB_LINK_FLAGS} -Wl,--end-group
 
 
